6971a7c55f6a558ec01843092c7738bf663c0444
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-04-11 21:21:41 -0300

    removing comments

--- src/Program.java
+++ src/Program.java
@@ -10,7 +10,7 @@ public class Program {
         Retangulo retangulo = new Retangulo(12, 10);
 
 
-        //figuraInvalida : pega da entrada usuario ou define inicialmente?
+        //figuraInvalida
         String figura = "cone";
 
 
@@ -39,50 +39,5 @@ public class Program {
         System.out.println(quadrado.toString());
         System.out.println(retangulo.toString());
 
-
-
-/*
-        int per = fig.p(FigurasGeometricas.R);
-        if (per == -234 || per == -2658) {
-            System.out.println("ERRO");
-        }
-
-        per = fig.p(FigurasGeometricas.C);
-        if (per == -234 || per == -2658) {
-            System.out.println("ERRO");
-        }
-
-        per = fig.p(-2);
-        if (per == -234 || per == -2658) {
-            System.out.println("ERRO");
-        }
-
-        per = fig.p(0);
-        if (per == -234 || per == -2658) {
-            System.out.println("ERRO");
-        }
-*/
-
-/*
-        fig.a(0);
-        fig.a(-1);
-        fig.a(2);
-        fig.a(1);
-        fig.a(-2);
-
- */
-
-        /*
-        String inf = fig.toStringDaFigura(-1);
-        inf = fig.toStringDaFigura(1);
-        System.out.println(inf);
-        inf = fig.toStringDaFigura(0);
-        System.out.println(inf);
-        inf = fig.toStringDaFigura(4);
-        System.out.println(inf);
-        inf = fig.toStringDaFigura(2);
-        System.out.println(inf);
-
-         */
     }
 }
